b3a7104130440e2179c82d71fa39e5b48155775d
diulv <danielclaywillmott@gmail.com>
2015-05-26 16:05:59 -0400

    slugies?

--- rango/migrations/0003_category_slug.py
+++ rango/migrations/0003_category_slug.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='category',
             name='slug',
-            field=models.SlugField(null=True, blank=True),
+            field=models.SlugField(unique=True),
             preserve_default=False,
         ),
     ]
