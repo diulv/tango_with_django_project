a23622070c3482e99d1f509f70fc742efb8597f3
diulv <danielclaywillmott@gmail.com>
2015-05-27 14:22:31 -0400

    deleted unnecessary stuff

---
+++ rango/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+	name = forms.CharField(max_length=128, help_text="Enter category name")
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+	
+	class Meta:
+		model = Category
+		fields = ('name',)
+		
+class PageForm(forms.ModelForm):
+	title = forms.CharField(max_length=128, help_text="Please enter title")
+	url = forms.URLField(max_length=200, help_text="Enter URL")
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	
+	class Meta:
+		model = Page
+		exclude = ('category',)
+		
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+		
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			clearned_data['url'] = url
+		
+		return cleaned_data
