b9611fc34f11ed98021448b0d60ddece76057c25
diulv <danielclaywillmott@gmail.com>
2015-05-27 09:22:52 -0400

    added top 5 pages to index.html

--- rango/views.py
+++ rango/views.py
@@ -25,4 +25,7 @@ def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	context_dict = {'categories': category_list}
 	
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict['pages'] = page_list
+	
 	return render(request, 'rango/index.html', context_dict)
