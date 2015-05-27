61986ffa40c5b0ade4e0ff1704d8590230378203
diulv <danielclaywillmott@gmail.com>
2015-05-27 16:20:08 -0400

    added registration page (still need login, on 9.5)

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 		url(r'^add_category/$', views.add_category, name='add_category'),
 		url(r'^category/(?P<category_name_slug>\w+)/add_page/$', views.add_page, name='add_page'),
 		url(r'^category/(?P<category_name_url>[\w\-]+)/$', views.category, name='category'),  # New!
+		url(r'^register/$', views.register, name='register'),
 	)
