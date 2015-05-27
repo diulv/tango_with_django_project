b741326cb14a8fe6fc559681f6c90e2d8130a447
diulv <danielclaywillmott@gmail.com>
2015-05-27 10:47:21 -0400

    added forms for new category page

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm
+
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:	
+			print form.errors
+	else:
+		form = CategoryForm()
+		
+	return render(request, 'rango/add_category.html', {'form': form})
 
 def category(request, category_name_slug):
 
